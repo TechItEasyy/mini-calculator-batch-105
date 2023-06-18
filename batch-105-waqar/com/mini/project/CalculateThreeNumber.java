96eb62b1a97ee8b8e42207d40bc0a7df706fd6be
Waqar Sarwar <waqarsarwar38@gmail.com>
2023-06-18 17:35:50 +0530

    deleted

--- batch-105-waqar/com/mini/project/CalculateThreeNumber.java
+++
@@ -1,6 +0,0 @@
-package com.mini.project;
-
-public interface CalculateThreeNumber {
-	public int calculate(int a ,int b, int c);
-
-}
--- batch-105-waqar/com/mini/project/CalculateTwoNumber.java
+++
@@ -1,7 +0,0 @@
-package com.mini.project;
-
-public interface CalculateTwoNumber {
-	public int calculate(int a ,int b);
-	
-
-}
