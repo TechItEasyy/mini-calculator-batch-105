02792ee32d67617089d4a152a5c152f8c2c011e9
shahidr0909 <mdshahidraja0909@gmail.com>
2023-06-19 22:41:58 +0530

    Shahid

---
+++ minicalculator/DivideNumber.java
@@ -0,0 +1,17 @@
+package com.calculater.project;
+
+public class DivideNumber implements Calculator
+  {
+	@Override
+	public int calculate(int a , int b) {
+		int div;
+		div = a/b;
+		return div;
+
+ }
+
+	@Override
+	public int calculate(int a, int b, int c) {
+		return 0;
+	}
+}
---
+++ minicalculator/SubstractNumber.java
@@ -0,0 +1,17 @@
+package com.calculater.project;
+
+public class SubstractNumber implements Calculator
+  {
+	@Override
+	public int calculate(int a , int b) {
+		int sub;
+		sub = a - b;
+		return sub;
+	}
+
+	@Override
+	public int calculate(int a, int b, int c) {
+		return 0;
+	}
+
+}
