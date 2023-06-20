a9d19fb085328b1bea02819378c4588ef01f7bd3
imtiyaz98 <talktoimtiaz94@gmail.com>
2023-06-20 09:15:25 +0530

    minicalculator for 2 and 3

---
+++ batch-105-Imtiyaz/Calculator.java
@@ -0,0 +1,14 @@
+package com.day14;
+
+
+public interface Calculator 
+{
+	//Calculate two numbers.
+	
+	public int calculate(int a , int b);
+	
+	//Calculate three numbers.
+	public int calculate(int a, int b, int c);
+	
+}
+
---
+++ batch-105-Imtiyaz/DivideNumber.java
@@ -0,0 +1,17 @@
+package com.day14;
+
+public class DivideNumber implements Calculator
+{
+	
+	public int calculate(int a , int b) {
+		int div;
+		div = a/b;
+		return div;
+
+}
+
+	
+	public int calculate(int a, int b, int c) {
+		return 0;
+	}
+}
